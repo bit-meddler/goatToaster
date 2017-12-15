f0c6da6c9c5049eb3afe8672dd6a72016b63fa83
RichH <RichH@beacon-optical.co.uk>
2017-12-15 12:53:35 +0000

    VBO Code

--- primative.py
+++ primative.py
@@ -16,8 +16,8 @@ import OpenGL.GLUT
 import numpy as np
 
 import gtMath as GM
+import chromaTool as CT
 import gtHelpers
-
 
 class Primative( object ):
     """
@@ -39,7 +39,7 @@ class Primative( object ):
         
     """
     def __init__( self, name, gt_parent )
-        self.X = np.zeros( (4,4), dtype=GM.FLOAT )
+        self.X = np.eye( 4, dtype=GM.FLOAT )
         self.dirty = False
         self.name = name
         self._p = gt_parent
@@ -60,6 +60,8 @@ class Displayable( Primative ):
     def __init__( self, name, gt_parent ):
         super( Displayable, self ).__init__( name, parent )
         self.setName( name )
+        # gl VBO stuff
+        self._VBO = None
         # Overidable
         self._label_os   = (2.5, 2.5, 0.) # Label Offset
         self._draw_scale = 1.0 # draw scale
@@ -97,9 +99,29 @@ class Displayable( Primative ):
             t += self._label_os
             glRasterPos3f(*t)
 			GLUT.glutBitmapString(self.font, name)
-            
-            
+        
+        
+    def vboInit( self ):
+        self._VBO = GLuint( 0 )
+        glGenBuffers( 1, self._VBO )
+        self._VBO = self._VBO.value
+        glBindBuffer( GL_ARRAY_BUFFER_ARB, self._VBO )
+        
+        
+    def vboPopulate( self, buff_data, vbo_mode=GL_STATIC_DRAW ):
+        # https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glBufferData.xhtml#description
+        # GL_STATIC_DRAW will be most popular
+        glBufferData( GL_ARRAY_BUFFER, buff_data.nbytes, buff_data, vbo_mode )
+        glBindBuffer( GL_ARRAY_BUFFER_ARB, self._VBO )
+        glVertexPointer( 3, GL_FLOAT, 0, None )
+        
+        
+    def vboDestroy( self ):
+        glDeleteBuffers(1, GLuint( self._VBO ) )
+        
+        
 class FloorGrid( Displayable ):
+
     def __init__( self, mjr_dims=(3,3) mjr_spacing=50., mnr_spacing=25., gt_parent ):
         super( FloorGrid, self).__init__( "", gt_parent )
         # Don't draw axis or label
@@ -111,4 +133,37 @@ class FloorGrid( Displayable ):
         # calculate junctions of Major lines
         v,i,c,a = gtHelpers.makeGrid( self.mjr_dims[0], self.mjr_dims[1],
                                       self.mjr_spacing, lambda f1,f2:(f1, 0.01, f2) )
-        # TODO
+        self._verts = v
+        self._grid_idx = i
+        self._floor_idx = c
+        self._axis_x = a[0]
+        self._axis_z = a[1]
+        # do vbbo
+        self.vboInit()
+        self.vboPopulate( self._verts )
+        
+        #Minor lines, TODO
+        # set colour
+        self._col_mjr   = CT.web24f( self._p.COLOURS["GRID"][0], 1.0  )
+        self._col_floor = CT.web24f( self._p.COLOURS["FLOOR"][0], 1.0 )
+        
+        
+    def draw( self ):
+        # Omiting super call - no axis or name for floor
+        # setup for drawing
+        glEnable( GL_BLEND )
+		# Bind verts into VBO
+        # Draw floor
+        pos = self.X[:3,3]
+        glColor4f( self._col_floor )
+        glTranslate( *pos )
+		# draw a quad
+        glEnableClientState( GL_VERTEX_ARRAY )
+        glDrawElementsui( GL_QUADS, self._floor_idx )
+        # Draw Gridlines MJR
+		glColor3f( self._col_mjr )
+		glLineWidth( 1 )
+		# draw lines
+        glDrawElementsui( GL_LINES, self._grid_idx )
+        # Reset GL OPTS
+        glDisableClientState( GL_VERTEX_ARRAY )
