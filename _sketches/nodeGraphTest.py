6f1ed2ad4c9abc1def9fa7b2c5c153e726b351e4
RichH <RichH@beacon-optical.co.uk>
2017-12-18 16:38:38 +0000

    Nieve split between UI slots and Internal slots

--- _sketches/nodeGraphTest.py
+++ _sketches/nodeGraphTest.py
@@ -8,11 +8,17 @@ class Node( object ):
     DIR_BI  =  0
     DIR_OUT = -1
     DIR_LUT = {
-        DIR_IN  :">-",
-        DIR_BI  :"<>",
-        DIR_OUT :"->",
+        DIR_IN  : ">-",
+        DIR_BI  : "<>",
+        DIR_OUT : "->",
     }
     
+    GRP_INT = 1
+    GRP_UI  = 2
+    GRP_LUT = {
+        GRP_INT : "Internal",
+        GRP_UI  : "Interface"
+    }
     
     def __init__( self, name, title ):
         self.inner_name = name
@@ -24,11 +30,11 @@ class Node( object ):
         self.dirty = False
         
         
-    def addSlot( self, name, type, direction ):
+    def addSlot( self, name, type, group, direction ):
         if not name in self.slots:
             self.slots[ name ] = None
             self.slotOrder.append( name )
-        self.slots[ name ] = [ type, direction ]
+        self.slots[ name ] = [ type, group, direction ]
         
         
     def invalidate( self ):
@@ -36,18 +42,26 @@ class Node( object ):
         for child in self.children:
             child.invalidate()
             
+            
     def process( self ):
         self.dirty = False
-        # ?? or should the SG manage this ??
-        for child in self.children:
-            child.process()
     
     
     def show( self ):
-        print "'{}' ({})".format( self.display_name, self.inner_name )
+        print "'{}' ({}) [{}Dirty]".format( self.display_name, self.inner_name,
+                                          "" if self.dirty else "Not " )
+        slots_ui  = ""
+        slots_int = ""
         for slot in self.slotOrder:
-            t, d = self.slots[slot]
-            print "\t{: >16}:{: >9} {}".format( slot, t, self.DIR_LUT[d] )
+            t, g, d = self.slots[slot]
+            if( g == self.GRP_UI ):
+                slots_ui += "\t{: >16}:{: >9} {}\n".format( slot, t, self.DIR_LUT[d] )
+            else:
+                slots_int+= "\t{: >16}:{: >9} {}\n".format( slot, t, self.DIR_LUT[d] )
+        print "Internal"
+        print slots_int
+        print "Interface"
+        print slots_ui
         print "\nDownstream:"
         for c in self.children:
             print "\t{}".format( c.display_name )
@@ -62,6 +76,7 @@ class Scene( object ):
         self.nodes = {}
         self.ids = 1000
         self.links = {}
+        self.visited = set()
 
         
     def addNode( self, name, title ):
@@ -83,9 +98,9 @@ class Scene( object ):
         if not target_slot in self.nodes[ target_name ].slots:
             return
         
-        if self.nodes[ source_name ].slots[ source_slot ][1] > 0:
+        if self.nodes[ source_name ].slots[ source_slot ][2] > Node.DIR_BI:
             return
-        if self.nodes[ target_name ].slots[ target_slot ][1] < 0:
+        if self.nodes[ target_name ].slots[ target_slot ][2] < Node.DIR_BI:
             return
         
         # finally, connect them
@@ -96,28 +111,54 @@ class Scene( object ):
         
         
     def show( self ):
+        print "Nodes"
         for _,n in self.nodes.iteritems():
             n.show()
-            print "--------------------------------------------"
+            print "-"*37
+        print "Edges"
+        for source, target in self.links.iteritems():
+            print "{} -> {}".format( source, target )
             
             
+    def _findNoParent( self ):
+        for n in self.nodes:
+            if len( n.parents ) == 0:
+                if not n in self.visited:
+                    self.visited.add( n )
+                    return n
+        return None
+        
+        
+    def walk( self ):
+        """ Walk the Graph.
+        """
+        self.visited = set()
+        vist = []
+        no_parents = []
+        task = self._findNoParent()
+        if task != None:
+            no_parents.insert( 0, task )
+        while( len(no_parents)>0 ):
+            pass
+        
+        
 if __name__ == "__main__":
     sg = Scene()
     
     blur = sg.addNode( "blur", "Blur Node" )
-    blur.addSlot( "frame_in", "IMAGE", Node.DIR_IN )
-    blur.addSlot( "frame_out", "IMAGE", Node.DIR_OUT )
-    blur.addSlot( "radius", "FLOAT", Node.DIR_BI )
-    blur.addSlot( "amount", "FLOAT", Node.DIR_BI )
+    blur.addSlot( "frame_in", "IMAGE", Node.GRP_INT, Node.DIR_IN )
+    blur.addSlot( "frame_out", "IMAGE", Node.GRP_INT, Node.DIR_OUT )
+    blur.addSlot( "radius", "FLOAT", Node.GRP_UI, Node.DIR_BI )
+    blur.addSlot( "amount", "FLOAT", Node.GRP_UI, Node.DIR_BI )
     
     read = sg.addNode( "read", "Read File" )
-    read.addSlot( "frame_out", "IMAGE", Node.DIR_OUT )
+    read.addSlot( "frame_out", "IMAGE", Node.GRP_INT, Node.DIR_OUT )
     
     edge = sg.addNode( "edge", "Edge Enhance Node" )
-    edge.addSlot( "frame_in", "IMAGE", Node.DIR_IN )
-    edge.addSlot( "frame_out", "IMAGE", Node.DIR_OUT )
-    edge.addSlot( "k_size", "DESCRETE", Node.DIR_BI )
-    edge.addSlot( "amount", "FLOAT", Node.DIR_BI )
+    edge.addSlot( "frame_in", "IMAGE", Node.GRP_INT, Node.DIR_IN )
+    edge.addSlot( "frame_out", "IMAGE", Node.GRP_INT, Node.DIR_OUT )
+    edge.addSlot( "k_size", "DESCRETE", Node.GRP_UI, Node.DIR_BI )
+    edge.addSlot( "amount", "FLOAT", Node.GRP_UI, Node.DIR_BI )
     
     sg.connect( read.inner_name, "frame_out", blur.inner_name, "frame_in" )
     sg.connect( blur.inner_name, "frame_out", edge.inner_name, "frame_in" )
